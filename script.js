0539a64c55d62416d184c59a06810da86a2e2d9c
megan cho <meganlisacho@gmail.com>
2021-03-28 17:36:01 -0500

    Added html pages, css, and JS

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+    fields.firstName = document.getElementById('firstName');
+    fields.lastName = document.getElementById('lastName');
+    fields.email = document.getElementById('email');
+    fields.question = document.getElementById('question');
+   })
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+   }
+
+
+   function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.firstName, isNotEmpty);
+    valid &= fieldValidation(fields.lastName, isNotEmpty);
+    valid &= fieldValidation(fields.question, isNotEmpty);
+   
+    return valid;
+   }
+
+class User {
+    constructor(firstName, lastName,email, question) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.question = question;
+    }
+   }
+
+function sendContact() {
+    if (isValid()) {
+        let usr = new User(firstName.value, lastName.value, email.value);
+        alert(`${$usr.firstName} Thanks for reaching out!`)
+    } else {
+        alert("There as was an error")  
+    }
+}
